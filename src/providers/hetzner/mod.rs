82ac949f14366009ca026f72501ce29b430382fd
Antonio Viggiano <agfviggiano@gmail.com>
2025-06-11 11:23:46 -0300

    Restore AWS provider and provider selection

--- src/providers/hetzner/mod.rs
+++ src/providers/hetzner/mod.rs
@@ -39,6 +39,13 @@ impl Provider for HetznerProvider {
             name: Some(name.to_string()),
         };
 
+        let ip = job.ipv4.clone();
+        let key_path = config.ssh_key_path.clone();
+        let provider = self.clone();
+        tokio::spawn(async move {
+            let _ = provider.install_dependencies(&ip, &key_path).await;
+        });
+
         Ok(job)
     }
 
@@ -151,6 +158,7 @@ impl Provider for HetznerProvider {
 fn config() -> Result<Config, Box<dyn std::error::Error + Send + Sync>> {
     config::load_config("./config.toml")
 }
+#[derive(Clone)]
 pub struct HetznerProvider {}
 
 impl HetznerProvider {
