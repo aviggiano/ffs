8baa8d543f37035c8f135cd3db5c7c6323839586
Antonio Guilherme Ferreira Viggiano <agfviggiano@gmail.com>
2025-08-09 09:59:25 -0300

    Update setup.sh

--- src/setup.sh
+++ src/setup.sh
@@ -1,3 +1,9 @@
+# Update and upgrade
+apt-get update
+apt-get upgrade -y
+
+# Install dependencies
+apt-get install -y unzip tree
 
 # Install Node.js
 curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash
@@ -8,8 +14,14 @@ curl -L https://foundry.paradigm.xyz | bash
 source /root/.bashrc
 foundryup
 
-# Install Echidna
+# Install Go
+wget https://go.dev/dl/go1.23.1.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.23.1.linux-amd64.tar.gz
 
+# Install Medusa
+go install github.com/crytic/medusa@latest
+
+# Install Echidna
 ECHIDNA_VERSION="2.2.6"
 wget https://github.com/crytic/echidna/releases/download/v${ECHIDNA_VERSION}/echidna-${ECHIDNA_VERSION}-x86_64-linux.tar.gz
 tar -xzvf echidna-${ECHIDNA_VERSION}-x86_64-linux.tar.gz
